1c9f20bb07e12e6f47e95bf00727998bb9bf902e
brantmerrell <brant.merrell@gmail.com>
2015-01-11 18:27:57 -0600

    here goes nothin

---
+++ project1.R
@@ -0,0 +1,45 @@
+data<-read.csv2("C:/users/josh/downloads/exdata-data-household_power_consumption/household_power_consumption.txt",
+                colClasses="character",
+                col.names=c("Date","Time","Global_Active_Power","Global_Reactive_Power",
+                            "Voltage","Global_Intensity",
+                            "Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+                nrows=as.integer(2075259*0.05))
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S") 
+data$Date<-strptime(data$Date, format="%d/%m/%Y")
+data<-data[data$Date=="2007-02-01" | 
+             data$Date=="2007-02-02",]
+data$Time<-strptime(data$Time, format="%H:%M:%S")
+data$Global_Active_Power<-as.numeric(data$Global_Active_Power)
+data$Global_Reactive_Power<-as.numeric(data$Global_Reactive_Power)
+data$Voltage<-as.numeric(data$Voltage)
+data$Global_Intensity<-as.numeric(data$Global_Intensity)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
+
+if(nrow(data)!=2880){warning("Data does not show (2days x 24hours x 60 minutes) 2880 observations")}
+
+par(mar=c(2,2,2,2), mfrow=c(1,1))
+with(data, hist(Global_Active_Power,
+                main="Global Active Power",
+                xlab="Global Active Power (kilowatts)",
+                ylab="Frequency",
+                col="red"))
+par(mar=c(1,1,1,1), mfrow=c(1,1))
+with(data,plot(Date, Global_Active_Power, 
+               type="l",
+               ylab="Global Active Power (kilowatts"))
+par(mar=c(1,1,1,1), mfrow=c(1,1))
+with(data, plot(Time, Sub_metering_1, "l"))
+with(data, lines(Time, Sub_metering_2, col = "red"))
+with(data, plot(Time, , "s"))
+with(data, plot(Time, Sub_metering_2, "s", col = "red"))
+with(data, lines(Time, Sub_metering_3, "s", col = "blue"))
+
+par(mar=c(1,1,1,1), mfrow=c(2,2))
+with(data, plot(Time, Global_Active_Power, "l"))
+with(data, plot(DateTime, Voltage, "l"))
+with(data, plot(Time, Sub_metering_1),
+     plot(Time, Sub_metering_2, col = "red"),
+     plot(Time, Sub_metering_3, col = "blue"))
+with(data, plot(DateTime, Global_Active_Power, "l"))
