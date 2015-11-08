c091748a4799d8e55a910c8c30f4afb335f4a836
Brant Merrell <brant.merrell@gmail.com>
2015-11-08 18:50:26 -0500

    basic update

--- project1.R
+++ project1.R
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
 
 png(filename="C:/users/josh/documents/exdata_plotting1/mfrow2x2.png")
 
-=======
->>>>>>> b1349fed160c88aaa4cfd3b85f0341a7eb845553
 data<-read.csv2("C:/users/josh/downloads/exdata-data-household_power_consumption/household_power_consumption.txt",
                 colClasses="character",
                 col.names=c("Date","Time","Global_Active_Power","Global_Reactive_Power",
                             "Voltage","Global_Intensity",
                             "Sub_metering_1","Sub_metering_2","Sub_metering_3"),
                 nrows=as.integer(2075259*0.05))
-<<<<<<< HEAD
-=======
 data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S") 
->>>>>>> b1349fed160c88aaa4cfd3b85f0341a7eb845553
 data$Date<-strptime(data$Date, format="%d/%m/%Y")
 data<-data[data$Date=="2007-02-01" | 
              data$Date=="2007-02-02",]
 data$Time<-strptime(data$Time, format="%H:%M:%S")
-<<<<<<< HEAD
 data$DateTime <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
-=======
->>>>>>> b1349fed160c88aaa4cfd3b85f0341a7eb845553
 data$Global_Active_Power<-as.numeric(data$Global_Active_Power)
 data$Global_Reactive_Power<-as.numeric(data$Global_Reactive_Power)
 data$Voltage<-as.numeric(data$Voltage)
@@ -40,7 +31,6 @@ with(data, hist(Global_Active_Power,
                 ylab="Frequency",
                 col="red"))
 dev.off()
-<<<<<<< HEAD
 png(filename="C:/users/josh/documents/exdata_plotting1/Plot 2.png")
 par(mar=c(2,4,2,4), oma=c(1,1,1,1), mfrow=c(1,1))
 with(data,plot(DateTime, Global_Active_Power, 
@@ -70,6 +60,3 @@ with(data, lines(DateTime, Sub_metering_2, col = "red"))
 with(data, lines(DateTime, Sub_metering_3, col = "blue"))
 with(data, plot(DateTime, Global_Active_Power, "l"))
 dev.off()
-=======
-
->>>>>>> b1349fed160c88aaa4cfd3b85f0341a7eb845553
