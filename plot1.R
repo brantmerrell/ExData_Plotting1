99b488e5c07bc600a83726fa375652e03380189f
Brant Merrell <brant.merrell@gmail.com>
2016-02-11 15:10:03 -0500

    plot1.R

--- plot1.R
+++ plot1.R
@@ -12,32 +12,23 @@ download.file(
 unzip("raw data (zip)")
 
 # read data
-consumption <- read.csv2("household_power_consumption.txt",
-                         na.strings = "?")
+consumption <- fread("household_power_consumption.txt",
+                     na.strings="?")
 
-# convert data to YYYY-MM-DD format
-consumption$Date <- strptime(consumption$Date, format="%d/%m/%Y")
+# convert data to class 'Date'
+consumption$Date <- as.Date(consumption$Date, format="%d/%m/%Y")
+
+fromDate <- as.Date("2007-02-01"); toDate   <- as.Date("2007-02-02")
 
 # subset dataset to target dates February 1 and 2, 2007
-consumption<-consumption[consumption$Date %in% c("2007-02-01","2007-02-02"),]
-
-# select color
-sampleColors <- sample(colors(),1)
-
-# inspect
-plot(length(sampleColors):1,
-     col = sampleColors)
-legend("right",
-       legend = sampleColors, 
-       fill = sampleColors)
-# if undesirable, resample
+consumption<-consumption[(consumption$Date >= fromDate & consumption$Date <= toDate),]
 
 # open graphics device
 png("plot1.png")
 
 # plot histogram of Global Active Power
 hist(as.numeric(consumption$Global_active_power), 
-     col = sampleColors,
+     col = "red",
      xlab = "Global Active Power (kilowatts)",
      main = "Global Active Power")
 
